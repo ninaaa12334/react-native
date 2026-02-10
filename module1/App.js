a203ddff889f14d469ac0a2a403758e4a141ca05
ninaaa12334 <160941480+ninaaa12334@users.noreply.github.com>
2026-02-10 16:56:56 +0100

    .

--- module1/App.js
+++ module1/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
+import { StatusBar } from 'react-native-web';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import {Stylesheet, View, Flatlist}
+import TestScreen from './screens/TestScreen';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <StudentInfoScreen />
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Test" component={TestScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
@@ -16,5 +19,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
