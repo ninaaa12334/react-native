a213e095f055873a298e7b20152524e2fcb7dbf5
ninaaa12334 <160941480+ninaaa12334@users.noreply.github.com>
2026-02-19 17:38:07 +0100

    .

--- module1/App.js
+++ module1/App.js
@@ -1,13 +1,9 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import ProfileScreen from './screens/ProfileScreen';
-import StudentInfo from './screens/StudentInfo';
-import Projects from './screens/Projects';
-import MainScreen from './screens/Main.Screen';import TestScreen from './screens/TestScreen';
-import StudentsScreen from './screens/StudentsScreen';
-
-import ProfileScreen from './screens/ProfileScreen';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import ProfileScreen from "./screens/ProfileScreen";
+import MainScreen from "./screens/MainScreen";
+import ButtonScreen from "./screens/ButtonScreen";  
 
 const Stack = createStackNavigator();
 
@@ -15,9 +11,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen 
-          name="Profile" 
-          component={ProfileScreen} 
+        <Stack.Screen
+          name="Profile"
+          component={ProfileScreen}
         />
       </Stack.Navigator>
     </NavigationContainer>
